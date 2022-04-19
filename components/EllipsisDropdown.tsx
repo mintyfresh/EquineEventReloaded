5a9bbd8cdab55c52db13ee1430c4bbb4f2cd6fd5
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2022-04-19 03:31:30 -0400

    Extract ellipsis-dropdown component.

---
+++ components/EllipsisDropdown.tsx
@@ -0,0 +1,30 @@
+import { faEllipsisH } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { forwardRef } from 'react';
+import { Dropdown } from 'react-bootstrap';
+
+const EllipsisToggle = forwardRef<HTMLAnchorElement, { onClick: (event: any) => void }>(({ onClick }, ref) => (
+  <a className="text-secondary" href="#" ref={ref} onClick={(event) => {
+    event.preventDefault();
+    onClick(event);
+  }}>
+    <FontAwesomeIcon icon={faEllipsisH} />
+  </a>
+));
+
+export interface EllipsisDropdownProps extends React.ComponentPropsWithoutRef<typeof Dropdown> {
+  children: React.ReactNode;
+}
+
+const EllipsisDropdown: React.FC<EllipsisDropdownProps> = ({ children, ...props }) => {
+  return (
+    <Dropdown {...props}>
+      <Dropdown.Toggle as={EllipsisToggle} />
+      <Dropdown.Menu>
+        {children}
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
+
+export default EllipsisDropdown;
--- components/PlayerList/PlayerControls.tsx
+++ components/PlayerList/PlayerControls.tsx
@@ -1,16 +1,6 @@
-import { forwardRef } from 'react';
 import { Dropdown } from 'react-bootstrap';
-import { FaEllipsisH } from 'react-icons/fa';
 import type { Player } from '../../lib/players';
-
-const EllipsisToggle = forwardRef<HTMLAnchorElement, { onClick: (event: any) => void }>(({ onClick }, ref) => (
-  <a className="text-secondary" href="#" ref={ref} onClick={(event) => {
-    event.preventDefault();
-    onClick(event);
-  }}>
-    <FaEllipsisH />
-  </a>
-));
+import EllipsisDropdown from '../EllipsisDropdown';
 
 interface PlayerControlsProps {
   player: Player;
@@ -18,15 +8,12 @@ interface PlayerControlsProps {
 
 const PlayerControls: React.FC<PlayerControlsProps> = ({ player }) => {
   return (
-    <Dropdown className="float-end">
-      <Dropdown.Toggle as={EllipsisToggle} />
-      <Dropdown.Menu>
-        <Dropdown.Item>Mark unpaid</Dropdown.Item>
-        <Dropdown.Item>Drop</Dropdown.Item>
-        <Dropdown.Divider />
-        <Dropdown.Item className="text-danger">Remove</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+    <EllipsisDropdown className="float-end">
+      <Dropdown.Item>Mark unpaid</Dropdown.Item>
+      <Dropdown.Item>Drop</Dropdown.Item>
+      <Dropdown.Divider />
+      <Dropdown.Item className="text-danger">Remove</Dropdown.Item>
+    </EllipsisDropdown>
   );
 };
 
