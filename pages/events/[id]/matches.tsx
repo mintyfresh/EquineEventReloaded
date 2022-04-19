8bc041c7a08acad74b476134cf002f08133d796a
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2022-04-19 00:55:47 -0400

    Add placeholder content to matches page.

--- pages/events/[id]/matches.tsx
+++ pages/events/[id]/matches.tsx
@@ -1,5 +1,6 @@
 import type { GetServerSideProps } from 'next';
 import type { ReactElement } from 'react';
+import { Button, ButtonToolbar, Card, Col, Form, Row } from 'react-bootstrap';
 import EventLayout from '../../../components/EventLayout';
 import { Event, getEvent } from '../../../lib/events';
 import type { NextPageWithLayout } from '../../../types/next-page';
@@ -22,7 +23,30 @@ interface EventMatchesPageProps {
 
 const EventMatchesPage: NextPageWithLayout = () => {
   return (
-    <></>
+    <>
+      <h2>Matches</h2>
+      <Row className="mb-3">
+        <Col xs="auto">
+          <Form.Select>
+            <option>All</option>
+            <option>Current</option>
+            <option>Completed</option>
+          </Form.Select>
+        </Col>
+        <Col xs="auto" className="ms-auto">
+          <ButtonToolbar className="gap-2">
+            <Button variant="outline-secondary">Pair Next Match</Button>
+            <Button variant="outline-secondary">Pair Current Match</Button>
+            <Button variant="outline-secondary">Unpair Last Round</Button>
+          </ButtonToolbar>
+        </Col>
+      </Row>
+      <Card body className="text-center">
+        <Card.Text>
+          No matches yet.
+        </Card.Text>
+      </Card>
+    </>
   );
 };
 
