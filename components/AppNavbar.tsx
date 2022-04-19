382ebd1e7600686d3c80afff8cd09e0fcc07f647
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2022-04-18 22:20:57 -0400

    Add stub events page.

--- components/AppNavbar.tsx
+++ components/AppNavbar.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Container, Navbar } from 'react-bootstrap'
+import { Container, Nav, Navbar } from 'react-bootstrap'
 
 interface AppNavbarProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'onSelect'> {
 }
@@ -11,6 +11,14 @@ const AppNavbar: React.FC<AppNavbarProps> = ({ ...props }) => {
         <Link href="/" passHref>
           <Navbar.Brand>Equine Event Runner</Navbar.Brand>
         </Link>
+        <Navbar.Toggle aria-controls="app-navbar-nav" />
+        <Navbar.Collapse id="app-navbar-nav">
+          <Nav className="mr-auto">
+            <Link href="/events" passHref>
+              <Nav.Link>Events</Nav.Link>
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
---
+++ pages/events/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const EventsIndexPage: NextPage = () => {
+  return (
+    <>
+      <h1>Events</h1>
+    </>
+  );
+};
+
+export default EventsIndexPage;
