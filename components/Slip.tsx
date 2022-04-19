dee4c5fe1f0c7ef773797e816785df3bffdd5fe1
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2022-04-19 08:22:40 -0400

    Fill in missing player names.

--- components/Slip.tsx
+++ components/Slip.tsx
@@ -2,6 +2,10 @@ import type { Event } from '../lib/events';
 import { Match } from '../lib/matches';
 import { RankedPlayer } from '../lib/rankings';
 
+const playerName = (player: RankedPlayer | undefined) => (
+  player?.name ?? 'None'
+);
+
 export interface SlipProps {
   event: Event;
   match: Match;
@@ -17,18 +21,18 @@ const Slip: React.FC<SlipProps> = ({ event, match, players }) => {
       <text transform="matrix(1 0 0 1 216.8911 13.5493)" fontFamily="'MyriadPro-Regular'" fontSize="12">Win</text>
       <text transform="matrix(1 0 0 1 368.0713 13.5493)" fontFamily="'MyriadPro-Regular'" fontSize="12">First</text>
       <text transform="matrix(1 0 0 1 458.9805 13.5493)" fontFamily="'MyriadPro-Regular'" fontSize="12">Drop?</text>
-      <text transform="matrix(1 0 0 1 35.5 37.624)" fontFamily="'MyriadPro-Regular'" fontSize="12">{players[0]?.points || 0} - {players[0]?.name}</text>
+      <text transform="matrix(1 0 0 1 35.5 37.624)" fontFamily="'MyriadPro-Regular'" fontSize="12">{players[0]?.points || 0} - {playerName(players[0])}</text>
       <text transform="matrix(1 0 0 1 205.6055 37.624)" fontFamily="'MyriadPro-Regular'" fontSize="12">_______</text>
       <text transform="matrix(1 0 0 1 357.5654 37.624)" fontFamily="'MyriadPro-Regular'" fontSize="12">_______</text>
       <text transform="matrix(1 0 0 1 453.0225 37.624)" fontFamily="'MyriadPro-Regular'" fontSize="12">_______</text>
       <text transform="matrix(1 0 0 1 290.3911 13.5493)" fontFamily="'MyriadPro-Regular'" fontSize="12">Tie</text>
       <text transform="matrix(1 0 0 1 281.1055 54.625)" fontFamily="'MyriadPro-Regular'" fontSize="12">_______</text>
-      <text transform="matrix(1 0 0 1 35.5 69.1245)" fontFamily="'MyriadPro-Regular'" fontSize="12">{players[1]?.points || 0} - {players[1]?.name}</text>
+      <text transform="matrix(1 0 0 1 35.5 69.1245)" fontFamily="'MyriadPro-Regular'" fontSize="12">{players[1]?.points || 0} - {playerName(players[1])}</text>
       <text transform="matrix(1 0 0 1 205.6055 69.1245)" fontFamily="'MyriadPro-Regular'" fontSize="12">_______</text>
       <text transform="matrix(1 0 0 1 357.5654 69.1245)" fontFamily="'MyriadPro-Regular'" fontSize="12">_______</text>
       <text transform="matrix(1 0 0 1 453.0225 69.1245)" fontFamily="'MyriadPro-Regular'" fontSize="12">_______</text>
-      <text transform="matrix(1 0 0 1 71 103.2485)" fontFamily="'MyriadPro-Regular'" fontSize="12">{players[0]?.name}: _______________________</text>
-      <text transform="matrix(1 0 0 1 289 103.2485)" fontFamily="'MyriadPro-Regular'" fontSize="12">{players[1]?.name}: _______________________</text>
+      <text transform="matrix(1 0 0 1 71 103.2485)" fontFamily="'MyriadPro-Regular'" fontSize="12">{playerName(players[0])}: _______________________</text>
+      <text transform="matrix(1 0 0 1 289 103.2485)" fontFamily="'MyriadPro-Regular'" fontSize="12">{playerName(players[1])}: _______________________</text>
       <text transform="matrix(1 0 0 1 2.999 110.0005)" fontFamily="'MyriadPro-Regular'" fontSize="12">{match.table}</text>
     </svg>
   );
