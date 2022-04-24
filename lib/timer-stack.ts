02290b0b81f21edf70fee4b56a101ec0f218239a
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2022-04-23 23:41:40 -0400

    Implement timer.

---
+++ lib/timer-stack.ts
@@ -0,0 +1,81 @@
+import { useState, useEffect } from 'react';
+
+const ONE_MINUTE = 60 * 1000;
+
+export interface Timer<T> {
+  duration: number;
+  data: T;
+}
+
+export const useTimerStack = function<T>({ onExpired }: { onExpired?: (data: T) => void }) {
+  const [isActive, setActive] = useState(false);
+  const [timers, setTimers] = useState<Timer<T>[]>([]);
+  const [remaining, setRemaining] = useState(0);
+  const [intervalID, setIntervalID] = useState<number | null>(null);
+
+  const timer = timers[0];
+
+  const intervalHandler = () => {
+    setRemaining((remaining) => remaining - 1000);
+  };
+
+  useEffect(() => {
+    if (isActive && timers.length === 0) {
+      setActive(false);
+    }
+  }, [isActive, timers]);
+
+  useEffect(() => {
+    if (isActive && !intervalID) {
+      setIntervalID(window.setInterval(intervalHandler, 1000));
+      return;
+    }
+
+    if (!isActive && intervalID) {
+      window.clearInterval(intervalID);
+      setIntervalID(null);
+    }
+  }, [isActive, intervalID]);
+
+  useEffect(() => {
+    if (timers.length > 0 && remaining <= 0) {
+      const nextTimer = timers[1];
+
+      if (nextTimer) {
+        setRemaining(nextTimer.duration);
+      }
+
+      setTimers(timers.slice(1));
+      onExpired?.(timers[0].data);
+    }
+  }, [remaining, timers]);
+
+  const pushTimer = (duration: number, data: T) => {
+    setTimers((timers) => [...timers, { duration, data }]);
+    setRemaining((remaining) => remaining || duration);
+    setActive(true);
+  };
+
+  const clearTimers = () => {
+    setActive(false);
+    setRemaining(0);
+    setTimers([]);
+  };
+
+  const seconds = timer ? Math.floor((remaining / 1000) % 60) : 0;
+  const minutes = timer ? Math.floor((remaining / ONE_MINUTE) % 60) : 0;
+  const hours   = timer ? Math.floor((remaining / ONE_MINUTE / 60) % 24) : 0;
+  const days    = timer ? Math.floor((remaining / ONE_MINUTE / 60 / 24)) : 0;
+
+  return {
+    remaining,
+    seconds,
+    minutes,
+    hours,
+    days,
+    isActive,
+    pushTimer,
+    clearTimers,
+    data: timer?.data
+  };
+};
--- pages/timer.tsx
+++ pages/timer.tsx
@@ -1,14 +1,85 @@
 import Head from 'next/head';
+import { useState } from 'react';
+import { Button, ButtonToolbar, Container, Form } from 'react-bootstrap';
+import { useTimerStack } from '../lib/timer-stack';
 import type { NextPageWithLayout } from '../types/next-page';
 
+const ONE_MINUTE = 60 * 1000;
+const SETUP_DURATION = ONE_MINUTE * 3;
+
+interface TimerData {
+  label: string;
+  audio?: string;
+}
+
 const TimerPage: NextPageWithLayout = () => {
+  const [skipSetup, setSkipSetup] = useState(false);
+
+  const { data, isActive, minutes, seconds, pushTimer, clearTimers } = useTimerStack<TimerData>({
+    onExpired: ({ audio }) => {
+      audio && new Audio(audio).play();
+    }
+  });
+
+  const pushSetupTimer = () => {
+    pushTimer(skipSetup ? 0 : SETUP_DURATION, { label: 'Setup', audio: 'sounds/begin.wav' });
+  };
+
   return (
     <>
       <Head>
         <title>Timer - Equine Event Runner</title>
       </Head>
-      <h1>Timer</h1>
-      TODO
+      <Container className="pt-3">
+        <ButtonToolbar className="gap-2">
+          <Button
+            variant="outline-secondary"
+            disabled={isActive}
+            onClick={() => {
+              pushSetupTimer();
+              pushTimer(35 * ONE_MINUTE, { label: 'Time Remaining', audio: 'sounds/over.wav' });
+              pushTimer(5 * ONE_MINUTE, { label: 'Games Ends In', audio: 'sounds/time.wav' });
+            }}
+          >
+            35 minutes
+          </Button>
+          <Button
+            variant="outline-secondary"
+            disabled={isActive}
+            onClick={() => {
+              pushSetupTimer();
+              pushTimer(45 * ONE_MINUTE, { label: 'Time Remaining', audio: 'sounds/over.wav' });
+            }}
+          >
+            45 minutes
+          </Button>
+          <Form.Check
+            type="switch"
+            id="timer-skip-setup"
+            label="Skip setup"
+            className="my-auto"
+            disabled={isActive}
+            checked={skipSetup}
+            onChange={(event) => setSkipSetup(event.currentTarget.checked)}
+          />
+          <Button
+            variant="outline-danger"
+            className="ms-auto"
+            disabled={!isActive}
+            onClick={() => clearTimers()}
+          >
+            Stop
+          </Button>
+        </ButtonToolbar>
+        <div className="position-absolute top-50 start-50 translate-middle text-center">
+          <div style={{ fontSize: '75px' }} className="display-1">
+            {data?.label || 'Time Remaining'}
+          </div>
+          <div style={{ fontSize: '200px' }}>
+            {minutes.toString().padStart(2, '0')} : {seconds.toString().padStart(2, '0')}
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
