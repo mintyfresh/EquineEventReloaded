03f7eb60dc94d137064e97f7efd598216cb07356
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2022-04-24 01:16:26 -0400

    Add sorting to players table.

--- components/PlayerList.tsx
+++ components/PlayerList.tsx
@@ -1,6 +1,49 @@
+import { faSort, faSortAsc, faSortDesc } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { sortBy } from 'lodash';
+import React, { useState } from 'react';
 import { Table } from 'react-bootstrap';
 import type { Player, UpdateEventPlayerInput } from '../api/types';
 import PlayerListItem from './PlayerList/PlayerListItem';
+
+function useSortableColumns<T>(): [
+  (values: T[]) => T[],
+  React.FC<{ column: keyof T, defaultSortDesc?: boolean, children: React.ReactChild }>
+] {
+  const [sortByColumn, setSortByColumn] = useState<keyof T | null>(null);
+  const [sortByAsc, setSortByAsc] = useState(true);
+
+  return [
+    (values: T[]) => {
+      if (sortByColumn) {
+        values = sortBy(values, sortByColumn);
+
+        if (!sortByAsc) {
+          values = values.reverse();
+        }
+      }
+
+      return values;
+    },
+    ({ column, defaultSortDesc = false, children }) => (
+      <th role="button" onClick={() => {
+        if (sortByColumn === column) {
+          setSortByAsc(!sortByAsc);
+        } else {
+          setSortByColumn(column)
+          setSortByAsc(!defaultSortDesc);
+        }
+      }}>
+        {sortByColumn === column ? (
+          <FontAwesomeIcon icon={sortByAsc ? faSortAsc : faSortDesc} className="me-1" />
+        ) : (
+          <FontAwesomeIcon icon={faSort} className="me-1" />
+        )}
+        {children}
+      </th>
+    )
+  ];
+}
 
 export interface PlayerListProps {
   players: Player[];
@@ -9,21 +52,35 @@ export interface PlayerListProps {
 };
 
 const PlayerList: React.FC<PlayerListProps> = ({ players, onPlayerUpdate, onPlayerDelete }) => {
+  const [sortPlayers, SortableColumnHeader] = useSortableColumns<Player>();
+
   return (
-    <Table>
+    <Table variant="striped">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Wins</th>
-          <th>Losses</th>
-          <th>Ties</th>
-          <th>Points</th>
-          <th>Opponent Win %</th>
+          <SortableColumnHeader column="name">
+            Name
+          </SortableColumnHeader>
+          <SortableColumnHeader column="wins" defaultSortDesc={true}>
+            Wins
+          </SortableColumnHeader>
+          <SortableColumnHeader column="losses" defaultSortDesc={true}>
+            Losses
+          </SortableColumnHeader>
+          <SortableColumnHeader column="ties" defaultSortDesc={true}>
+            Ties
+          </SortableColumnHeader>
+          <SortableColumnHeader column="points" defaultSortDesc={true}>
+            Points
+          </SortableColumnHeader>
+          <SortableColumnHeader column="opponentWinPercentage" defaultSortDesc={true}>
+            Opponent Win %
+          </SortableColumnHeader>
           <th className="text-end">Controls</th>
         </tr>
       </thead>
       <tbody>
-        {players.map((player) => (
+        {sortPlayers(players).map((player) => (
           <PlayerListItem
             key={player.id}
             player={player}
